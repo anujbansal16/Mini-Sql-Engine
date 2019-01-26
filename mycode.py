2f51b795f14074f1a7009a9c074eb9cd1fc5c572
Anuj Bansal <anuj.bansal16@gmail.com>
2019-01-26 23:23:50 +0530

    Worked on one table

--- mycode.py
+++ mycode.py
@@ -1,4 +1,6 @@
 import sys
+import re
+import csv
 import pprint
 import numpy as np
 import sqlparse as parser
@@ -10,7 +12,8 @@ whereClause=None
 identifiers=None
 distinct=False
 aggregate=None
-aggregateFunctions=["SUM", "AVERAGE", "MAX","MIN"]
+aggregateFunctions={"SUM":sum, "AVERAGE":"anuj", "MAX":max,"MIN":min}
+tableData={}
 
 def printError(s):
 	print(s)
@@ -20,7 +23,8 @@ def main():
 	if len(sys.argv)>1:
 		#read meta data file
 		readMetaData()
-		# loadTablest()
+		loadTables()
+		# pprint.pprint(tableData)
 		#read command line
 		# print(tableDict)
 		sqlQueryStmt=parser.parse(sys.argv[1])[0]
@@ -28,25 +32,164 @@ def main():
 			print("Success")
 			executeQuery()
 		else:
-			printError("Something goes wrong")
-		# print(tables,identifiers,whereClause,distinct,aggregate)
+			printError("")
 
 	else:
 		print("Provide sql query as argument")
 
+def  getAggregateData(aggregate):
+	index=aggregate.find("(")
+	return (aggregate[:index],aggregate[index+1:len(aggregate)-1])
+
 def executeQuery():
-	print(tables,identifiers,whereClause,distinct,aggregate)
+	print("tables",tables,"\nidentifiers= ",identifiers,"\nwhereClause= ",whereClause,"\ndistinct= ",distinct,"\naggregate= ",aggregate)
 	print(tableDict)
 	for table in tables:
 		if table not in tableDict.keys():
 			return printError("Error: %s table doesn't exist "%(table))
-	# if len(tables)==1:
-		
+	dataDict={}
+	################################### single table######################
+	if len(tables)==1:
+
+		tableName=tables[0]
+		keys=tableData[tableName].keys()
+		if len(identifiers)==1 and identifiers[0]=="*":
+			printOutput(tableData[tableName])
+		else:
+			if aggregate:
+				#aggregate present
+				aggFunt,aggAttr=getAggregateData(aggregate)
+				if aggAttr not in keys:
+					return printError("Invalid attribute "+aggAttr)
+				if(aggFunt=="AVERAGE"):
+					val=sum(tableData[tableName][aggAttr])/len(tableData[tableName][aggAttr])
+					dataDict[aggregate]=[val]
+					printOutput(dataDict)
+				else:
+					funct=aggregateFunctions[aggFunt]
+					val=funct(tableData[tableName][aggAttr])
+					dataDict[aggregate]=[val]
+					printOutput(dataDict)
+			else:
+				if distinct:
+					for attr in identifiers:
+						if attr not in keys:
+							return printError("Invalid attribute "+attr)
+					
+					length=len(tableData[tableName][identifiers[0]])
+					mylist=[]
+					for i in range(0,length):
+						temp=[]
+						for key in identifiers:
+							temp.append(tableData[tableName][key][i])
+						if temp not in mylist:
+							mylist.append(temp)
+					printRows([ table+"."+ident for ident in identifiers],mylist)
+
+				else:
+					for attr in identifiers:
+						if attr not in keys:
+							return printError("Invalid attribute "+attr)
+						else:
+							dataDict[tables[0]+"."+attr]=tableData[tableName][attr]
+					printOutput(dataDict)
+	################################### single table######################
+
+	################################### multiple table ######################
+	else:
+		# tableName=tables[0]
+		# keys=tableData.keys()
+		dataDict={}
+		if len(identifiers)==1 and identifiers[0]=="*":
+			for tableName in tables:
+				for attr in tableData[tableName].keys():
+					dataDict[tableName+"."+attr]=tableData[tableName][attr]
+			printOutput(dataDict)
+		else:
+			if aggregate:
+				#aggregate present
+				aggFunt,aggAttr=getAggregateData(aggregate)
+				if aggAttr not in keys:
+					return printError("Invalid attribute "+aggAttr)
+				if(aggFunt=="AVERAGE"):
+					val=sum(tableData[tableName][aggAttr])/len(tableData[tableName][aggAttr])
+					dataDict[aggregate]=[val]
+					printOutput(dataDict)
+				else:
+					funct=aggregateFunctions[aggFunt]
+					val=funct(tableData[tableName][aggAttr])
+					dataDict[aggregate]=[val]
+					printOutput(dataDict)
+			else:
+				if distinct:
+					for attr in identifiers:
+						if attr not in keys:
+							return printError("Invalid attribute "+attr)
+					
+					length=len(tableData[tableName][identifiers[0]])
+					mylist=[]
+					for i in range(0,length):
+						temp=[]
+						for key in identifiers:
+							temp.append(tableData[tableName][key][i])
+						if temp not in mylist:
+							mylist.append(temp)
+					printRows([ table+"."+ident for ident in identifiers],mylist)
+
+				else:
+					for attr in identifiers:
+						if attr not in keys:
+							return printError("Invalid attribute "+attr)
+						else:
+							dataDict[tables[0]+"."+attr]=tableData[tableName][attr]
+					printOutput(dataDict)
+
+
+
+def printRows(attrs,listRows):
+	print("=================================================================")
+	for a in attrs:
+		print("%15s  |"%(a),end=" ")
+	print()
+	print("=================================================================")
+	for row in listRows:
+		for val in row:
+			print("%15s  |"%(val),end=" ")
+		print()
+	print("=================================================================")
+
+
+
+def printOutput(dataDict):
+	print("=================================================================")
+	pprint.pprint(dataDict)
+	keys=list(dataDict.keys())
+	
+	# data=[]
+	# for key in keys:
+	# 	data.append(dataDict[key])
+	# # print(data)
+	# lis = []
+	# for lis in zip(i for i in data):
+	# 	print(lis[0])
+	# 	# print(temp[0][0])
+
+	for key in keys:
+		print("%15s  |"%(key),end=" ")
+	print()
+	print("=================================================================")
+	length=len(dataDict[keys[0]])
+	for i in range(0,length):
+		for key in keys:
+			print("%15s  |"%(dataDict[key][i]),end=" ")
+			# print(dataDict[key][i],"   |", end=" ")
+		print()
+	print("=================================================================")
 
 
 
 def validateQuery(tokens,tableDict):
-	pprint.pprint(tokens)
+	# pprint.pprint(tokens)
 	global tables
 	global whereClause
 	global identifiers
@@ -118,7 +261,7 @@ def extractTableIdentifiers(tokens):
 				tables.append(item.value.upper())
 			return (True,tables,i)
 		elif isinstance(tokenitem,Identifier):
-			tables.append(tokenitem.value)
+			tables.append(tokenitem.value.upper())
 			return (True,tables,i)
 		elif tokenitem.ttype is Wildcard:
 			tables.append(tokenitem.value.upper())
@@ -136,16 +279,32 @@ def readMetaData():
 		elif "end_table" in line:
 			table=False
 		elif table:
-			tableName=line.strip().upper()
+			tableName=line.strip()#.upper()
 			tableDict[tableName]=[]
 			table=False
 		else:
-			tableDict[tableName].append(tableName+"."+line.strip().upper())
+			# col=tableName.upper()+"."+line.strip().upper()
+			col=line.strip().upper()
+			tableDict[tableName].append(col)
 		line = fp.readline()
 
 def loadTables():
+	global tableDict
+	global tableData
+	# print("=============================")
 	for table in tableDict:
-		pd
+		with open(table+".csv") as csvFile:
+			# print(table)
+			temp={}
+			for col in tableDict[table]:
+				temp[col]=[]
+			csv_reader = csv.reader(csvFile, delimiter=',')
+			for row in csv_reader:
+				for index,val in enumerate(row):
+					temp[tableDict[table][index]].append(int(val))
+		# print("===========================")
+		tableData[table.upper()]=temp
+	tableDict={key.upper():tableDict[key] for key in tableDict.keys()}
 
 if __name__ == "__main__":
 	main()
